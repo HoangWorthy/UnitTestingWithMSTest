b3602ac58cf1a4c7ba6767dda6f62640b476930a
HoangWorthy <hoangsusp@gmail.com>
2025-05-13 17:57:54 +0700

    Adding comments for clarity

--- UserService.Tests/UserService_Tests1.cs
+++ UserService.Tests/UserService_Tests1.cs
@@ -20,9 +20,11 @@ namespace Test.UnitTests1
         [TestMethod]
         public void AddUser_ValidUser_AddsToList()
         {
+            // Add two users
             _service.AddUser("Khoi", "khoi@example.com");
             _service.AddUser("Hoang", "Hoang@example.com");
 
+            // The users should be added in order
             Assert.AreEqual(2, _service.Users.Count);
             Assert.AreEqual("Khoi", _service.Users[0].Name);
             Assert.AreEqual("Hoang", _service.Users[1].Name);
@@ -31,8 +33,10 @@ namespace Test.UnitTests1
         [TestMethod]
         public void AddUser_DuplicateName_ThrowsException()
         {
+            // Add one user
             _service.AddUser("Khoi", "khoi@example.com");
 
+            // Adding a user with the same name should throw an exception
             Assert.ThrowsException<InvalidOperationException>(() =>
             {
                 _service.AddUser("Khoi", "another@example.com");
@@ -42,11 +46,13 @@ namespace Test.UnitTests1
         [TestMethod]
         public void AddUser_EmptyNameOrEmail_ThrowsException()
         {
+            // Empty name should throw an ArgumentException
             Assert.ThrowsException<ArgumentException>(() =>
             {
                 _service.AddUser("", "email@example.com");
             });
 
+            // Empty email should also throw an ArgumentException
             Assert.ThrowsException<ArgumentException>(() =>
             {
                 _service.AddUser("Name", "");
--- UserService.Tests/UserService_Tests2.cs
+++ UserService.Tests/UserService_Tests2.cs
@@ -14,15 +14,19 @@ namespace Test.UnitTests2
         {
             _service = new UserService();
         }
+
         // === FindUserByName Tests ===
 
         [TestMethod]
         public void FindUserByName_ExistingUser_ReturnsUser()
         {
+            // Add a user so we can later find them
             _service.AddUser("Hoang", "hoang@example.com");
 
+            // Try to find the user by name
             var user = _service.FindUserByName("Hoang");
 
+            // Verify that a user was found and the email is correct
             Assert.IsNotNull(user);
             Assert.AreEqual("hoang@example.com", user!.Email);
         }
@@ -30,8 +34,10 @@ namespace Test.UnitTests2
         [TestMethod]
         public void FindUserByName_NonExistentUser_ReturnsNull()
         {
+            // Try to find a user that doesn't exist
             var user = _service.FindUserByName("NonExistent");
 
+            // Make sure the result is null since the user was never added
             Assert.IsNull(user);
         }
     }
