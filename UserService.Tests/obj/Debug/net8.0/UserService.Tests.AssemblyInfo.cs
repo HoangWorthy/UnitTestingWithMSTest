767cdd59e2b1c7f9342531d55639f6b2d486d138
HoangWorthy <hoangsusp@gmail.com>
2025-05-13 18:07:26 +0700

    Testing workflows

--- UserService.Tests/obj/Debug/net8.0/UserService.Tests.AssemblyInfo.cs
+++ UserService.Tests/obj/Debug/net8.0/UserService.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3602ac58cf1a4c7ba6767dda6f62640b476930a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27facb01e7a06f3ce6190359b7a51e1508eb6bc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
